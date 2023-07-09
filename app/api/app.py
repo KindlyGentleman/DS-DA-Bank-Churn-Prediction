ee4bafaf9ca994f7d65cc850ed8301befacca642
kindlygentleman <muhammadkamal52@gmail.com>
2023-07-09 15:18:41 +0700

    feat (app): add Docker support and Streamlit app

--- app/api/app.py
+++ app/api/app.py
@@ -1,25 +1,33 @@
+# Import necessary libraries
 import pickle
-from flask import Flask, request, app, jsonify,url_for,render_template
-import numpy as np
+from flask import Flask, request, jsonify, render_template
 import pandas as pd
 from utilities import prediction_pipeline
 
-app=Flask(__name__)
+# Create Flask app instance
+app = Flask(__name__)
 
+# Define route for home page
 @app.route('/')
 def home():
+    # Render home page template
     return render_template("home.html")
 
-@app.route('/predict_api',methods=['POST'])
+# Define route for prediction API
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data=request.json['data']
-    print(data)
-    data_new = pd.DataFrame(data, index = [0])
-    output= prediction_pipeline(data_new)
+    # Get data from request JSON
+    data = request.json['data']
+    # Convert data to DataFrame
+    data_new = pd.DataFrame(data, index=[0])
+    # Run prediction pipeline on data
+    output = prediction_pipeline(data_new)
+    # Return prediction result as JSON response
     if output[0] == 1:
         return jsonify("Churn")
     else:
         return jsonify("No Churn")
 
-if __name__ == "__main__": 
-    app.run(host="0.0.0.0",debug=True)
+# Run Flask app if script is run directly
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
